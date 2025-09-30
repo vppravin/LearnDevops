1656ae57027c0d4c5c57b58ff7c4942572e37e87
vppravin <88415893+vppravin@users.noreply.github.com>
2025-09-30 20:18:05 +0530

    Created index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Jenkins + Docker!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
