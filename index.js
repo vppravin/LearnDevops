9f40b160f1d34668b8862dc0ae1d42bcff06f981
vppravin <88415893+vppravin@users.noreply.github.com>
2025-10-01 18:09:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHad(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('HEYO USERS \n Hello from Jenkins + Docker using Poll SCM !');
 });
 
